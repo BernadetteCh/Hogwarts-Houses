20c86dda03f2583a38c49d06ce24938bd05327c0
Bernadette Christiner <b.christiner93@gmail.com>
2023-05-15 20:39:15 +0200

    initial commit

---
+++ src/test/java/com/example/hogwartshouses/HogwartsHousesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hogwartshouses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogwartsHousesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
