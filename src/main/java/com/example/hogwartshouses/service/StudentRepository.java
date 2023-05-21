15f277617fd259efac583d33af03e4ee541f6d4c
Bernadette Christiner <b.christiner93@gmail.com>
2023-05-21 17:53:36 +0200

    Implement interface for data manipulation

---
+++ src/main/java/com/example/hogwartshouses/service/StudentRepository.java
@@ -0,0 +1,26 @@
+package com.example.hogwartshouses.service;
+
+import com.example.hogwartshouses.model.Student;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface StudentRepository extends JpaRepository<Student,Long> {
+
+    @Query("SELECT s FROM Student s where s.firstName =:name and s.hasRoom=false ")
+    Student findByNameAndHasRoomNot(@Param("name") String name);
+
+    @Modifying()
+    @Transactional
+    @Query(value = "UPDATE Student s SET s.room.id = :roomId, s.hasRoom = true WHERE s.firstName= :name")
+    Integer findById(long roomId,String name);
+
+    @Modifying
+    @Transactional
+    @Query(value="UPDATE Room r SET r.availableBeds = r.availableBeds-1  WHERE r.id = :roomId")
+    Integer findById(long roomId);
+}
