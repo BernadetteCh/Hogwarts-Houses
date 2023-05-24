aeaf1a2e76f50050695949f92359cc148fc254df
Bernadette Christiner <b.christiner93@gmail.com>
2023-05-24 22:23:03 +0200

    Delegate logic to studentService class

--- src/main/java/com/example/hogwartshouses/endpoint/endpoints.java
+++ src/main/java/com/example/hogwartshouses/endpoint/endpoints.java
@@ -2,59 +2,44 @@ package com.example.hogwartshouses.endpoint;
 
 
 import com.example.hogwartshouses.model.*;
-import com.example.hogwartshouses.service.RoomRepository;
-import com.example.hogwartshouses.service.StudentRepository;
+import com.example.hogwartshouses.service.HogwartsService;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/")
+@CrossOrigin(origins = "*")
 public class endpoints {
+    private final HogwartsService studentService;
 
-    private final RoomRepository roomRepository;
-    private final StudentRepository studentRepository;
-
-    public endpoints(RoomRepository roomRepository, StudentRepository studentRepository) {
-        this.roomRepository = roomRepository;
-        this.studentRepository = studentRepository;
+    public endpoints(HogwartsService studentService) {
+        this.studentService = studentService;
     }
 
     @GetMapping("empty-rooms")
     public List<Room> getAllEmptyRooms(){
-        return roomRepository.findAll();
+        return studentService.findAllRooms();
     }
 
     @PostMapping("create-room")
     public Room saveRoom(@RequestBody Room room) {
-        return roomRepository.save(room);
+        return studentService.saveRoom(room);
+
     }
 
     @PostMapping("create-student")
     public Student saveStudent(@RequestBody Student student) {
-        return studentRepository.save(student);
+        return studentService.saveStudent(student);
+
     }
 
     @PutMapping("assignStudent/{name}")
     public Integer assignStudentToHouse(@PathVariable String name){
-        Student student = studentRepository.findByNameAndHasRoomNot(name);
-        Optional<Long> roomId = roomRepository.findFirstBy(student.getGender(), student.getHouseType());
-        studentRepository.findById(roomId.get());
-        return studentRepository.findById(roomId.get(), name);
+        return studentService.assignStudent(name);
     }
 
     @PutMapping("rooms/rat-owners/{name}")
     public Integer assignRonToHouse(@PathVariable String name) {
-        Student student = studentRepository.findByNameAndHasRoomNot(name);
-        Optional<Room> room = roomRepository.findRoomBy(student.getGender(),student.getHouseType());
-        Optional<Boolean> cat = room.get().getStudents().stream()
-                .map(student1 -> student1.getHousePet().equals(HousePet.CAT)).findFirst();
-        if(cat.get().booleanValue() == false) {
-            studentRepository.findById(room.get().getId());
-            return studentRepository.findById(room.get().getId(), name);
-        }else {
-            return null;
-        }
+       return studentService.findARoomForRatOwner(name);
     }
 }
