29bceeab1ff39930972b38d7e30efae971beaef4
Bernadette Christiner <b.christiner93@gmail.com>
2023-06-08 13:10:41 +0200

    Add getter and hashCode

--- src/main/java/com/example/hogwartshouses/model/Student.java
+++ src/main/java/com/example/hogwartshouses/model/Student.java
@@ -3,6 +3,8 @@ package com.example.hogwartshouses.model;
 
 import jakarta.persistence.*;
 
+import java.util.Objects;
+
 @Entity
 @Table(name="students")
 public class Student {
@@ -11,13 +13,12 @@ public class Student {
     private long id;
     private String firstName;
     private String lastName;
+
     private HousePet housePet;
+
     private HouseType houseType;
     private Gender gender;
-
-    public Gender getGender() {
-        return gender;
-    }
+    private boolean hasRoom;
 
     @ManyToOne
     @JoinColumn(name="room_id")
@@ -34,6 +35,23 @@ public class Student {
 
     public Student() {
 
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
     }
 
     public long getId() {
@@ -84,5 +102,31 @@ public class Student {
         this.hasRoom = hasRoom;
     }
 
-    private boolean hasRoom;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return id == student.id && hasRoom == student.hasRoom && firstName.equals(student.firstName) && lastName.equals(student.lastName) && housePet == student.housePet && houseType == student.houseType && gender == student.gender && room.equals(student.room);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, housePet, houseType, gender, hasRoom, room);
+    }
+
+    /*
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", housePet=" + housePet +
+                ", houseType=" + houseType +
+                ", gender=" + gender +
+                ", hasRoom=" + hasRoom +
+                ", room=" + room +
+                '}';
+    }*/
 }
