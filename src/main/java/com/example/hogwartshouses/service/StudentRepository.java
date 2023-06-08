086df94dc63b83a3a0795051fb26df79833f82cb
Bernadette Christiner <b.christiner93@gmail.com>
2023-06-08 13:11:01 +0200

    Refactor query

--- src/main/java/com/example/hogwartshouses/service/StudentRepository.java
+++ src/main/java/com/example/hogwartshouses/service/StudentRepository.java
@@ -10,16 +10,16 @@ import org.springframework.data.repository.query.Param;
 
 public interface StudentRepository extends JpaRepository<Student,Long> {
 
-    @Query("SELECT s FROM Student s where s.firstName =:name and s.hasRoom=false ")
-    Student findByNameAndHasRoomNot(@Param("name") String name);
+    Student findStudentByFirstNameAndHasRoomFalse(String name);
 
     @Modifying
     @Transactional
-    @Query(value = "UPDATE Student s SET s.room.id = :roomId, s.hasRoom = true WHERE s.firstName= :name")
-    Integer findById(long roomId,String name);
+    @Query(value = "UPDATE Student s SET s.room.id = :roomId, s.hasRoom = true WHERE s.firstName= :firstName")
+    Integer updateStudent(@Param("roomId")long roomId,@Param("firstName")String firstName);
 
     @Modifying
     @Transactional
-    @Query(value="UPDATE Room r SET r.availableBeds = r.availableBeds-1  WHERE r.id = :roomId")
-    Integer findById(long roomId);
+    @Query(value = "UPDATE Student s SET s.room.id = :roomId, s.hasRoom = false WHERE s.firstName= :firstName")
+    Integer assignStudentToRoom(@Param("roomId") long roomId, @Param("firstName") String firstName);
+
 }
