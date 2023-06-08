ddcf25cf1e4aaba62da8049d32abce8a1a9653ca
Bernadette Christiner <b.christiner93@gmail.com>
2023-06-08 13:10:08 +0200

    Refactor query

--- src/main/java/com/example/hogwartshouses/service/RoomRepository.java
+++ src/main/java/com/example/hogwartshouses/service/RoomRepository.java
@@ -3,23 +3,24 @@ package com.example.hogwartshouses.service;
 import com.example.hogwartshouses.model.Gender;
 import com.example.hogwartshouses.model.HouseType;
 import com.example.hogwartshouses.model.Room;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.web.bind.annotation.PathVariable;
+
 
 import java.util.List;
 import java.util.Optional;
 
 public interface RoomRepository extends JpaRepository<Room, Long> {
 
-    @Query("SELECT r FROM Room r where r.availableBeds >0")
-    List<Room> findAll();
-    @Query("SELECT r.id FROM Room r where r.gender = :gender and r.houseType = :houseType and r.availableBeds > 0")
-    Optional<Long> findFirstBy(@Param("gender") Gender gender, @Param("houseType") HouseType houseType);
+    Optional<Room> findRoomByGenderAndHouseType(Gender gender, HouseType houseType);
 
-    @Query("SELECT r FROM Room r where r.houseType = :houseType and r.gender = :gender")
-    Optional<Room> findRoomBy(@Param("gender") Gender gender, @Param("houseType") HouseType houseType);
+    @Modifying
+    @Transactional
+    @Query(value="UPDATE Room r SET r.availableBeds = r.availableBeds-1  WHERE r.id = :roomId")
+    Integer updateRoom(@Param("roomId") long roomId);
 }
 
 
