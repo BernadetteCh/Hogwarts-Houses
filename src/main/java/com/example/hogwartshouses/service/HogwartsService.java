520f814ad9a20aa1e395c140c143537403ea4f35
Bernadette Christiner <b.christiner93@gmail.com>
2023-06-08 13:12:56 +0200

    No relevant changes

--- src/main/java/com/example/hogwartshouses/service/HogwartsService.java
+++ src/main/java/com/example/hogwartshouses/service/HogwartsService.java
@@ -10,20 +10,19 @@ import java.util.Optional;
 
 @Service
 public class HogwartsService {
-
     private final StudentRepository studentRepository;
     private final RoomRepository roomRepository;
 
     public HogwartsService(StudentRepository studentRepository, RoomRepository roomRepository) {
         this.studentRepository = studentRepository;
         this.roomRepository = roomRepository;
+
     }
 
     public Integer assignStudent(String name) {
-        Student student = studentRepository.findByNameAndHasRoomNot(name);
-        Optional<Long> roomId = roomRepository.findFirstBy(student.getGender(), student.getHouseType());
-        studentRepository.findById(roomId.get());
-        return studentRepository.findById(roomId.get(), name);
+        Student student = studentRepository.findStudentByFirstNameAndHasRoomFalse(name);
+        Optional<Room> room = roomRepository.findRoomByGenderAndHouseType(student.getGender(), student.getHouseType());
+        return room.isPresent() ? studentRepository.assignStudentToRoom(room.get().getId(),name) : null;
     }
 
     public Student saveStudent(Student student) {
@@ -34,20 +33,26 @@ public class HogwartsService {
         return roomRepository.save(room);
     }
 
+
     public List<Room> findAllRooms() {
         return roomRepository.findAll();
     }
 
+
     public Integer findARoomForRatOwner(String name) {
-        Student student = studentRepository.findByNameAndHasRoomNot(name);
-        Optional<Room> room = roomRepository.findRoomBy(student.getGender(),student.getHouseType());
+        Student student = studentRepository.findStudentByFirstNameAndHasRoomFalse(name);
+        Optional<Room> room = roomRepository.findRoomByGenderAndHouseType(student.getGender(),student.getHouseType());
+        System.out.println(room.get());
         Optional<Boolean> cat = room.get().getStudents().stream()
                 .map(student1 -> student1.getHousePet().equals(HousePet.CAT)).findFirst();
+        System.out.println(cat.get().booleanValue()+"!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1");
         if(cat.get().booleanValue() == false) {
             studentRepository.findById(room.get().getId());
-            return studentRepository.findById(room.get().getId(), name);
+            roomRepository.updateRoom(room.get().getId());
+            return studentRepository.assignStudentToRoom(room.get().getId(), student.getFirstName());
         }else {
             return null;
         }
     }
+
 }
